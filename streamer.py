7b96f227a2c568bdf60f615de50ee5e5df11e0fa
kushaljamadagni <55068834+kushaljamadagni@users.noreply.github.com>
2021-12-05 22:23:07 +0530

    Update streamer.py

--- streamer.py
+++ streamer.py
@@ -1,1 +1,23 @@
-
+import json
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+from pyspark.streaming import StreamingContext
+from pyspark.ml.feature import Tokenizer,StopWordsRemover, CountVectorizer,IDF,StringIndexer
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.linalg import Vector
+from pyspark.sql.functions import length
+from pyspark.ml import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
+import numpy as np
+from sklearn.linear_model import SGDClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+sparkc=SparkContext("local[2]", "BigDataproject")
+sparkstreamc=StreamingContext(sparkc,1)
+spark = SparkSession.builder.getOrCreate()
